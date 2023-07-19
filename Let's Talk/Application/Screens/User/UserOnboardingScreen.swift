a3831809c5ceaf283d0ae61a4f78f4d6b5988037
Bbambangg95 <79217162+Bbambangg95@users.noreply.github.com>
2023-07-19 22:38:56 +0700

    fix: refactor HeroView, change Button to ButtonView

--- Let's Talk/Application/Screens/User/UserOnboardingScreen.swift
+++ Let's Talk/Application/Screens/User/UserOnboardingScreen.swift
@@ -10,31 +10,23 @@ import SwiftUI
 struct UserOnboardingScreen: View {
     var body: some View {
         LayoutView {
-            VStack {
-                
-                Spacer()
-                HStack {
-                    Image(systemName: "person")
-                        .offset(y: -40)
-                    Image(systemName: "person")
-                        .offset(y: 40)
-                }
-                .font(Font.system(size: 150, weight: .regular, design: .rounded))
-                .frame(maxHeight: 300)
-                Spacer()
-                Text("Welcome and Let’s Talk")
-                    .font(Font.system(size: 16, weight: .bold, design: .rounded))
-                    .padding(.vertical, 20)
-                Text("Where love and communication come together to create stronger and more meaningful relationships")
-                    .font(Font.system(size: 16, weight: .regular, design: .rounded))
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
-                    .padding(.bottom, 100)
-                Button("Next", action: {
-                    // Add action here
-                })
-                .buttonStyle(PrimaryButtonStyle())
+            Spacer()
+            HeroView(maleHero: "person.fill", femaleHero: "person")
+            Spacer()
+            Text("Welcome and Let’s Talk")
+                .font(Font.system(size: 16, weight: .bold, design: .rounded))
+                .padding(.vertical, 20)
+            Text("Where love and communication come together to create stronger and more meaningful relationships")
+                .font(Font.system(size: 16, weight: .regular, design: .rounded))
+                .multilineTextAlignment(.center)
+                .padding(.horizontal)
+                .padding(.bottom, 100)
+            ButtonView {
+                //
+            } label: {
+                Text("Next")
             }
+            .buttonStyle(.primary)
         }
     }
 }
