1d938d61158f4c76ba7fa533069a94a93dc1e984
Dewa Bisma <idewagedebisma@gmail.com>
2023-07-19 19:24:15 +0700

    feat: add button primary and secondary color to asset, finish butonstyle primary and secondary, finish button view

--- Let's Talk/Application/Styles/ButtonStyles.swift
+++ Let's Talk/Application/Styles/ButtonStyles.swift
@@ -5,4 +5,67 @@
 //  Created by Bisma Mahendra I Dewa Gede on 18/07/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct PrimaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(foregroundColor(for: configuration.role))
+            .frame(maxWidth: CGFloat.infinity)
+            .padding(Edge.Set.vertical, 17)
+            .background(color(for: configuration.role))
+            .cornerRadius(10)
+            .shadow(color: Color.black.opacity(0.25), radius: 1, x: 0, y: 4)
+    }
+    
+    func foregroundColor(for role: ButtonRole?) -> Color? {
+        role == .destructive || role == .cancel ? Color.white : Color.white
+    }
+    
+    func color(for role: ButtonRole?) -> Color {
+        switch role {
+        case .cancel?, .destructive?:
+            return Color.red
+        default:
+            return Color.buttonPrimary
+        }
+    }
+}
+
+extension ButtonStyle where Self == PrimaryButtonStyle {
+    static var primary: PrimaryButtonStyle {
+        PrimaryButtonStyle()
+    }
+}
+
+
+struct SecondaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .foregroundColor(foregroundColor(for: configuration.role))
+            .frame(maxWidth: CGFloat.infinity)
+            .padding(Edge.Set.vertical, 17)
+            .background(color(for: configuration.role))
+            .cornerRadius(10)
+            .shadow(color: Color.black.opacity(0.25), radius: 1, x: 0, y: 4)
+    }
+    
+    func foregroundColor(for role: ButtonRole?) -> Color? {
+        role == .destructive || role == .cancel ? Color.white : Color.white
+    }
+    
+    func color(for role: ButtonRole?) -> Color {
+        switch role {
+        case .cancel?, .destructive?:
+            return Color.red
+        default:
+            return Color.buttonSecondary
+        }
+    }
+}
+
+extension ButtonStyle where Self == SecondaryButtonStyle {
+    static var secondary: SecondaryButtonStyle {
+        SecondaryButtonStyle()
+    }
+}
