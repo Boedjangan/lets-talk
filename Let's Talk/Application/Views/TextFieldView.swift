ff1186b56f93cc4dd2ca142b5563dfcf9f62fb27
Dewa Bisma <idewagedebisma@gmail.com>
2023-07-19 23:41:37 +0700

    feat: finish text field view, add comment to view

---
+++ Let's Talk/Application/Views/TextFieldView.swift
@@ -0,0 +1,62 @@
+//
+//  TextFieldView.swift
+//  Let's Talk
+//
+//  Created by Bisma Mahendra I Dewa Gede on 19/07/23.
+//
+
+import SwiftUI
+import Combine
+
+/// A control view that displays an editable text interface with debounced value change.
+///
+/// Pass a binded state of String to the text parameter, when user change the text field value
+/// it will not immediately change the binded state, but there will be a delay until user stop
+/// typing before reading change to prevent unnecessary re-render because of state change
+///
+///     TextFieldView(text: $text, placeholder: "Placeholder")
+///
+struct TextFieldView: View {
+    @State var publisher = PassthroughSubject<String, Never>()
+    @State var debouncedText: String = ""
+    @Binding var text: String
+    
+    var debounceSeconds = 0.5
+    var placeholder: String
+    
+    var body: some View {
+        VStack() {
+            TextField("",
+                text: $debouncedText,
+                prompt: Text(placeholder).foregroundColor(.white)
+            )
+            .multilineTextAlignment(.center)
+            .font(Font.textInput)
+            .onChange(of: debouncedText) { value in
+                publisher.send(value)
+            }
+            .onReceive(
+                publisher.debounce(for: .seconds(debounceSeconds),scheduler: DispatchQueue.main)) { value in
+                    text = value
+                }
+            
+            Rectangle()
+                .fill(Color.white)
+                .frame(height: 0.5)
+        }
+        .onAppear{
+            debouncedText = text
+        }
+    }
+}
+
+struct TextFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.black
+            
+            TextFieldView(text: .constant(""), placeholder: "Namamu")
+                .foregroundColor(Color.white)
+        }
+    }
+}
