d77d527fc681096ebdfe4b2d3ab821393fd8b4ba
vindobangun8 <vindobangun8@gmail.com>
2023-07-20 12:15:27 +0700

    feat: finish Topic Icon View

---
+++ Let's Talk/Application/Screens/LoveLog/TopicIconView.swift
@@ -0,0 +1,29 @@
+//
+//  TopIconView.swift
+//  Let's Talk
+//
+//  Created by Datita Devindo Bahana on 20/07/23.
+//
+
+import SwiftUI
+
+struct TopIconView: View {
+    var foregroundColor:Color = Color.blue
+    var icon:String = "person.fill"
+    var body: some View {
+        ZStack{
+            Circle()
+                .foregroundColor(foregroundColor)
+                .frame(maxWidth: 65,maxHeight: 65)
+            Image(systemName: icon)
+                .foregroundColor(Color.white)
+                .font(.system(size: 32))
+        }
+    }
+}
+
+struct TopIconView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopIconView()
+    }
+}
