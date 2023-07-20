f2c9d8ac5aed095e9ce80cdc4322913b383faa13
vindobangun8 <vindobangun8@gmail.com>
2023-07-20 10:16:10 +0700

    feat: finsih Gender Selector View

--- Let's Talk/Application/Screens/User/Views/GenderSelectorView.swift
+++ Let's Talk/Application/Screens/User/Views/GenderSelectorView.swift
@@ -8,13 +8,63 @@
 import SwiftUI
 
 struct GenderSelectorView: View {
+    @Binding var gender:Int
+    @State private var rectanglePosition: CGPoint = CGPoint(x:-86,y:0)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.genderSelectorWrapper)
+                .frame (width: 344, height: 150)
+            
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.blue)
+                .frame (width: 172, height: 150)
+                .offset(x: rectanglePosition.x, y: rectanglePosition.y)
+            HStack{
+                Spacer()
+                VStack{
+                    Image(systemName: "person.fill")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 80))
+                        .padding(.bottom,10)
+                    Text("Male")
+                        .foregroundColor(gender == 0 ? Color.white: Color.black)
+                }
+                
+                Spacer()
+                VStack{
+                    Image(systemName: "person.fill")
+                        .foregroundColor(Color.white)
+                        .font(.system(size: 80))
+                        .padding(.bottom,10)
+                    Text("Female")
+                        .foregroundColor(gender == 1 ? Color.white: Color.black)
+                }
+                Spacer()
+            }
+//            .frame(maxWidth: 343, maxHeight:140)
+            
+//
+        }
+        .onTapGesture {
+                        // Update the rectanglePosition when the HStack is clicked
+                        withAnimation {
+                            if gender == 1 {
+                                gender = 0
+                                rectanglePosition = CGPoint(x: -86, y: 0)
+                            } else {
+                                gender = 1
+                                rectanglePosition = CGPoint(x: 86, y: 0)
+                            }
+                        }
+                    }
     }
 }
 
 struct GenderSelectorView_Previews: PreviewProvider {
     static var previews: some View {
-        GenderSelectorView()
+        GenderSelectorView(gender:.constant(0))
     }
 }
