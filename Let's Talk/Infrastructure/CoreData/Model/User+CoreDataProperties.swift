babb74888acb56a26f2a6cab69450b90c633e91a
Bbambangg95 <79217162+Bbambangg95@users.noreply.github.com>
2023-07-22 06:30:48 +0700

    feat: add core data entity, add entity relationship and array extension

---
+++ Let's Talk/Infrastructure/CoreData/Model/User+CoreDataProperties.swift
@@ -0,0 +1,48 @@
+//
+//  User+CoreDataProperties.swift
+//  Let's Talk
+//
+//  Created by Bambang Ardiyansyah on 22/07/23.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension User {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<User> {
+        return NSFetchRequest<User>(entityName: "User")
+    }
+
+    @NSManaged public var coupleId: String?
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var gender: Bool
+    @NSManaged public var talkDuration: Int16
+    @NSManaged public var updatedAt: Date?
+    @NSManaged public var username: String?
+    @NSManaged public var loveLog: NSSet?
+
+}
+
+// MARK: Generated accessors for loveLog
+extension User {
+
+    @objc(addLoveLogObject:)
+    @NSManaged public func addToLoveLog(_ value: LoveLog)
+
+    @objc(removeLoveLogObject:)
+    @NSManaged public func removeFromLoveLog(_ value: LoveLog)
+
+    @objc(addLoveLog:)
+    @NSManaged public func addToLoveLog(_ values: NSSet)
+
+    @objc(removeLoveLog:)
+    @NSManaged public func removeFromLoveLog(_ values: NSSet)
+
+}
+
+extension User : Identifiable {
+
+}
