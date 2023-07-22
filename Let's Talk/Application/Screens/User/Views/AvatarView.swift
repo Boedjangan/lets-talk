9f1392b5ae83bf496685b7e1012e461b210c3560
vindobangun8 <vindobangun8@gmail.com>
2023-07-22 11:01:52 +0700

    fix: buat optional value

--- Let's Talk/Application/Screens/User/Views/AvatarView.swift
+++ Let's Talk/Application/Screens/User/Views/AvatarView.swift
@@ -9,22 +9,30 @@ import SwiftUI
 
 struct AvatarView: View {
     var userName : String = ""
-//    var iconImage : Image = nil
+    var iconImage : String = ""
     var body: some View {
         VStack{
             ZStack{
                 Circle()
                     .foregroundColor(Color.avatarPlaceHolder)
                     .frame(width:141,height: 141)
-                Image(systemName: "person.fill")
-                    .foregroundColor(Color.black)
-                    .font(.system(size: 100))
+                if(iconImage == ""){
+                    Image(systemName: "person.fill")
+                        .foregroundColor(Color.black)
+                        .font(.system(size: 100))
+                }else{
+                    Image(iconImage)
+                        .font(.system(size: 100))
+                }
+               
             }
                 .padding(18)
+            if(userName != ""){
+                Text(userName)
+                    .font(Font.avatarIcon)
+                    .foregroundColor(Color.white)
+            }
             
-            Text(userName)
-                .font(Font.avatarIcon)
-                .foregroundColor(Color.white)
             
         }
     }
