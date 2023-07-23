96a3b77f3401af72198728c6b0fc9c56a86ee6bc
Bbambangg95 <79217162+Bbambangg95@users.noreply.github.com>
2023-07-23 14:24:18 +0700

    feat: finish AddQuestionMediaScreen

--- Let's Talk/Application/Screens/Question/AddQuestionMediaScreen.swift
+++ Let's Talk/Application/Screens/Question/AddQuestionMediaScreen.swift
@@ -10,11 +10,37 @@ import SwiftUI
 struct AddQuestionMediaScreen: View {
     var body: some View {
         LayoutView {
-            Text("Abadikan momen mengobrol kalian")
-                .font(.headingBig)
-            Text("Foto ini akan ditampilkan pada LoveLog untuk mengingat momen mengobrol kalian.")
-                .font(.paragraph)
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Abadikan momen mengobrol kalian")
+                    .font(.addMediaTitle)
+                Text("Foto ini akan ditampilkan pada LoveLog untuk mengingat momen mengobrol kalian.")
+                    .font(.paragraph)
+            }
+            CameraButtonView()
+            ButtonView {
+                //
+            } label: {
+                Text("Lanjut")
+            }
+            .buttonStyle(.fill(.primary))
+            
         }
+    }
+}
+
+struct CameraButtonView: View {
+    var body: some View {
+        ButtonView {
+            //
+        } label: {
+            Image(systemName: "camera.fill")
+                .font(.headingBig)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color("Ash"))
+        .cornerRadius(10)
+        .padding(.vertical)
+        .padding(.bottom, 20)
     }
 }
 
