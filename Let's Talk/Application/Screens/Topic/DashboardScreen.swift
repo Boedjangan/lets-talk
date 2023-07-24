fa26eebf851fef4051e5126de28648efdab2b46c
Dewa Bisma <idewagedebisma@gmail.com>
2023-07-24 18:47:13 +0700

    feat: create dashboard screen

--- Let's Talk/Application/Screens/Topic/DashboardScreen.swift
+++ Let's Talk/Application/Screens/Topic/DashboardScreen.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct DashboardScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        LayoutView(spacing: 40) {
+            TalkTimeView(talkTime: 0)
+            
+            // GreetingView here
+            
+            ScrollView {
+                VStack(spacing: 24) {
+                    TopicListView(progress: 0)
+                    
+                    TopicListView(level: "Level 2", progress: 0.25)
+                    
+                    TopicListView(level: "Level 3", progress: 0.50)
+                }
+            }
+        }
     }
 }
 
