c40dfc5a55c6dbcf568148a513a4b70f13af181a
Dewa Bisma <idewagedebisma@gmail.com>
2023-07-25 16:09:10 +0700

    feat: finish seeding script, add mock data

--- Let's Talk/Utils/JSONHandler.swift
+++ Let's Talk/Utils/JSONHandler.swift
@@ -7,24 +7,38 @@
 
 import Foundation
 
-func loadJson<T: Decodable>(_ filename: String) -> T {
+func loadJson<T: Decodable>(_ filename: String) -> T? {
     let data: Data
-
+    
     guard let file = Bundle.main.url(forResource: filename, withExtension: "json")
-        else {
-            fatalError("Couldn't find \(filename) in main bundle.")
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+        
+        return nil
     }
-
+    
     do {
         data = try Data(contentsOf: file)
     } catch {
         fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
     }
-
+    
     do {
         let decoder = JSONDecoder()
         return try decoder.decode(T.self, from: data)
     } catch {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+        
+        return nil
     }
+}
+
+func loadJSONAsDict(_ filename: String) -> [[String: Any]]? {
+    guard let url = Bundle.main.url(forResource: filename, withExtension: "json"),
+          let data = try? Data(contentsOf: url),
+          let jsonArray = try? JSONSerialization.jsonObject(with: data, options: []) as? [[String: Any]] else {
+        return nil
+    }
+    
+    return jsonArray
 }
