18cbcdf236d6229d99235ed72dcfd8ec67243f77
Elwin Johan Sibarani <elwinjohansibarani@10-63-32-76.0100.ilb.id.iosda.org>
2023-07-26 17:00:47 +0700

    feat:talk time view state

--- Let's Talk/Application/Screens/User/Views/TalkTimeView.swift
+++ Let's Talk/Application/Screens/User/Views/TalkTimeView.swift
@@ -9,25 +9,56 @@ import SwiftUI
 
 struct TalkTimeView: View {
     let talkTime: Int
+    let captionCard: String = "Ayo mulai tingkatkan waktu bersama kalian dengan menjawab pertanyaan!"
+    let captionCard2: String = "menit yang telah kalian habiskan bersama"
     
     var body: some View {
-        HStack {
+        HStack (alignment:.center){
             Text("\(talkTime)")
                 .font(.bigNumber)
                 .padding(.horizontal)
-            Text("Ayo mulai tingkatkan waktu bersama kalian dengan menjawab pertanyaan!")
+                .lineLimit(1)
+                .minimumScaleFactor(0.05)
+            VStack(alignment: .leading){
+                switch(talkTime){
+                case 1...45:
+                    renderLove(total: 1)
+                case 46...120:
+                    renderLove(total: 2)
+                case let x where x >= 121:
+                    renderLove(total: 3)
+                default:
+                    renderLove(total: 0)
+                }
+                Text(talkTime > 0 ? captionCard2 : captionCard)
+                    .font(.paragraph)
+            }
         }
         .foregroundColor(Color.talkTimeForeground)
-        .padding()
+        .padding(talkTime == 0 ? .all : .vertical,20)
+        .frame(maxWidth: 345, maxHeight: 117)
         .background(
             RoundedRectangle(cornerRadius: 20)
                 .foregroundColor(Color.talkTimeBackground)
+        )
+    }
+    
+    func renderLove(total: Int) -> some View{
+        return (
+            HStack (spacing: 0){
+                ForEach(0..<total, id: \.self){_ in
+                    return(
+                        Image(systemName: "heart.fill")
+                            .foregroundColor(.red)
+                    )
+                }
+            }
         )
     }
 }
 
 struct TalkTimeView_Previews: PreviewProvider {
     static var previews: some View {
-        TalkTimeView(talkTime: 20)
+        TalkTimeView(talkTime: 10)
     }
 }
